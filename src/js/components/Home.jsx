6a77f805e3c1a481d330d1bcec99e273d69ae57f
valvaradop <122133270+valvaradop@users.noreply.github.com>
2025-12-15 11:46:53 +0000

    todoval

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [task, setTask] = useState("")
+	const [array, setArray] = useState([])
+	const addTask = () => {
+		setArray(prev => [...prev, task])
+		console.log(array);
+	}
 	return (
 		<div className="text-center">
-            
+			<h1>Valeria ToDo List</h1>
+			<input
+				type="text"
+				value={task}
+				onChange={(e) => setTask(e.target.value)}
+			/>
+			<div>
+				<button onClick={() => addTask()} >Agregar Task</button>
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+			{
+				array.map((tarea) => {
+					return (
+						<div>
+							<ul className="list-group">
+								<li class="list-group-item">{tarea}</li>
+							</ul>
+						</div>
+					)
+				})
+			}
+
+
 		</div>
 	);
 };
